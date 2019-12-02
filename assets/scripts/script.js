387b2542d5f89cc0158fae90a79bc742104c676f
Jameskutty Sebastian <jameskutty.sebastian@gmail.com>
2019-12-03 01:25:28 +1030

    completed outline for html

---
+++ assets/scripts/script.js
@@ -0,0 +1,33 @@
+
+
+// var defaultURL = "https://api.openweathermap.org/data/2.5/weather?units=metric";
+// var city = "Adelaide";
+// var cityAdd = "&q="+city;
+// // var CurrentURL = "http://api.openweathermap.org/data/2.5/weather?units=metric"
+// var key = "&appid=ed08a362d4cb8955e89ed3cacd336637";
+// var queryDefaultURL = defaultURL+cityAdd+key;
+
+// $.ajax({
+//     url : queryDefaultURL,
+//     method : "GET"
+// }).then(function(response){
+//     // var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+//     console.log(response);
+// })
+
+
+
+// var Day5URL = "https://api.openweathermap.org/data/2.5/forecast?units=metric";
+// var city = "Melbourne";
+// var cityAdd = "&q="+city;
+// var key5Day = "&appid=0cc819ac1487bb114ac7b71d4c192ed7";
+// var queryURLDay5 = Day5URL+cityAdd+key5Day;
+
+// $.ajax({
+//     url : queryURLDay5,
+//     method : "GET"
+// }).then(function(response){
+//     var typ = response;
+//     console.log(typ);
+//     console.log(response);
+// })
