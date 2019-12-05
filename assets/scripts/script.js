25447a4ccdc3ace899a66d625de00ed6dd7d68cd
Jameskutty Sebastian <jameskutty.sebastian@gmail.com>
2019-12-05 11:44:24 +1030

    completed the saving of search history and showing on page load

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,9 +1,8 @@
-
+$(document).ready(function() {
 
 // var defaultURL = "https://api.openweathermap.org/data/2.5/weather?units=metric";
 // var city = "Adelaide";
 // var cityAdd = "&q="+city;
-// // var CurrentURL = "http://api.openweathermap.org/data/2.5/weather?units=metric"
 // var key = "&appid=ed08a362d4cb8955e89ed3cacd336637";
 // var queryDefaultURL = defaultURL+cityAdd+key;
 
@@ -11,10 +10,24 @@
 //     url : queryDefaultURL,
 //     method : "GET"
 // }).then(function(response){
-//     // var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
 //     console.log(response);
 // })
 
+
+//Search from searh box
+// var searchURL = "https://api.openweathermap.org/data/2.5/weather?units=metric";
+// var city = $("#inputCity").val();
+// var cityAdd = "&q="+city;
+// var key = "&appid=ed08a362d4cb8955e89ed3cacd336637";
+// var queryCurrentURL = searchURL+cityAdd+key;
+
+// $.ajax({
+//     url : queryCurrentURL,
+//     method : "GET"
+// }).then(function(response){
+//     // var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+//     console.log(response);
+// })
 
 
 // var Day5URL = "https://api.openweathermap.org/data/2.5/forecast?units=metric";
@@ -31,3 +44,68 @@
 //     console.log(typ);
 //     console.log(response);
 // })
+
+function populateSearchHistory(){
+    for (var i = 0; i < searchList.length; i++){
+        createSearchHistory(searchList[i]);
+    }    
+}
+
+function createSearchHistory(searchItem){
+    var searchContainer = $("<div>");
+    searchContainer.addClass("p-1 bg-light rounded rounded-pill shadow-sm mb-4 searchContainer");
+    $(".searchedItem").append(searchContainer);
+    var searchHistory = $("<div>");
+    searchHistory.addClass("searchHistory");
+    var searchItemTag = $("<p>");
+    searchItemTag.text(searchItem);
+    searchHistory.append(searchItemTag);
+    searchContainer.append(searchHistory);
+    
+    return("");
+}
+
+function createSearchedItemDiv (){
+    var searchedItem = $("<div>");
+    searchedItem.addClass("searchSection searchedItem");
+    $("#searchColumn").append(searchedItem);
+}
+var searchList = [];
+searchList = JSON.parse(localStorage.getItem("searchList"));
+
+if(searchList!==null){
+    createSearchedItemDiv();
+    populateSearchHistory();
+}
+
+
+$("#buttonSearch").click(function(){
+    var inputCity = $("#inputCity").val();
+    $("#inputCity").val("");
+    if(inputCity != ""){
+        $(".searchedItem").empty();
+        console.log(searchList);
+        if (searchList === null || searchList.length ==0){
+            searchList = [];
+            searchList.push(inputCity);
+            createSearchedItemDiv();
+        }
+        else{
+            searchList.splice(0, 0,  inputCity)
+        }        
+
+        if(searchList.length>8){
+            searchList.pop();
+            console.log(searchList);        
+        }
+        localStorage.setItem("searchList", JSON.stringify(searchList));
+        populateSearchHistory();
+        
+    }
+})
+
+
+
+
+
+})
